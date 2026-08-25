2e452adc1c56b58d4663e24ba619729c926ddc56
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-08-25 21:27:56 +0200

    Bump version

--- Fika.Headless/FikaHeadlessPlugin.cs
+++ Fika.Headless/FikaHeadlessPlugin.cs
@@ -47,7 +47,7 @@ namespace Fika.Headless;
 [BepInDependency("com.SPT.custom", BepInDependency.DependencyFlags.HardDependency)]
 public class FikaHeadlessPlugin : BaseUnityPlugin
 {
-    public const string HeadlessVersion = "1.5.1";
+    public const string HeadlessVersion = "1.5.2";
 
     public static FikaHeadlessPlugin Instance { get; private set; }
     public static ManualLogSource FikaHeadlessLogger;
